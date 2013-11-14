38c720a6a08bfef81bdfc6d6f1cf6f751f6d5ae9
Apprentice <apprentice@dbc-30.local>
2013-11-13 16:00:50 -0800

    Generate new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuclearFootball::Application.config.secret_token = '69a6365a0172d866325b8a93ae64ba1ce3b0df47bf3d83c1ec9b16f0764de553c06082c2bc7568c502dfb23279b05e6440536bf99a6a1a5ba2069fba7c4567f2'
